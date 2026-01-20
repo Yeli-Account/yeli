3659a1bc503445d738140fde5ce2fdf7cd9eb7fe
Yeli <OriginYech@outlook.com>
2026-01-20 18:42:07 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    
-    return new Response(await import('./index.html?raw'), {
-      headers: { 'content-type': 'text/html' }
-    });
-  }
-}
-
